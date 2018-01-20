e0ae716b9ed36af88ddf58cc244005114e9dbcb8
Gustav Westling <hello@gustav.tv>
2018-01-20 21:20:37 +0100

    fea(string) implement len() function

--- compiler/compiler/internal/string.go
+++ compiler/compiler/internal/string.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"github.com/llir/llvm/ir"
 	"github.com/llir/llvm/ir/types"
 )
 
@@ -9,4 +10,12 @@ func String() *types.StructType {
 		types.I32,                  // String length
 		types.NewPointer(types.I8), // Content
 	)
+}
+
+func StringLen(stringType types.Type) *ir.Function {
+	param := ir.NewParam("input", stringType)
+	res := ir.NewFunction("string_len", types.I32, param)
+	block := res.NewBlock("entry")
+	block.NewRet(block.NewExtractValue(param, []int64{0}))
+	return res
 }
