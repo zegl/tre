d523a600770458ebf1557d52d3f1a513b0fedf2f
Gustav Westling <hello@gustav.tv>
2018-02-01 21:08:25 +0100

    fea(compiler) support for 1 arg substring, runtime errors if out of range

--- compiler/compiler/internal/string.go
+++ compiler/compiler/internal/string.go
@@ -7,14 +7,14 @@ import (
 
 func String() *types.StructType {
 	return types.NewStruct(
-		types.I32,                  // String length
+		types.I64,                  // String length
 		types.NewPointer(types.I8), // Content
 	)
 }
 
 func StringLen(stringType types.Type) *ir.Function {
 	param := ir.NewParam("input", stringType)
-	res := ir.NewFunction("string_len", types.I32, param)
+	res := ir.NewFunction("string_len", types.I64, param)
 	block := res.NewBlock("entry")
 	block.NewRet(block.NewExtractValue(param, []int64{0}))
 	return res
