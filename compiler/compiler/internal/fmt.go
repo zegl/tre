a7d0098648a7ce76ed915018cb9e1829f833840a
Gustav Westling <hello@gustav.tv>
2018-02-03 15:52:50 +0100

    fea(compiler) add println function

---
+++ compiler/compiler/internal/fmt.go
@@ -0,0 +1,18 @@
+package internal
+
+import (
+	"github.com/llir/llvm/ir"
+	"github.com/llir/llvm/ir/types"
+	"github.com/zegl/tre/compiler/compiler/strings"
+)
+
+func Println(stringType types.Type, printfFunc *ir.Function, module *ir.Module) *ir.Function {
+	param := ir.NewParam("input", stringType)
+	res := ir.NewFunction("println", types.Void, param)
+	block := res.NewBlock("entry")
+	fmt := module.NewGlobalDef(strings.NextStringName(), strings.Constant("%s\n"))
+	fmt.IsConst = true
+	block.NewCall(printfFunc, strings.Toi8Ptr(block, fmt), strings.TreStringToi8Ptr(block, param))
+	block.NewRet(nil)
+	return res
+}
