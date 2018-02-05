41b4eb8daa5e8e383d5c8eb766b5874d80c3b81e
Gustav Westling <hello@gustav.tv>
2018-02-05 17:42:16 +0100

    fea(for) add break and continue statements

--- compiler/compiler/for.go
+++ compiler/compiler/for.go
@@ -13,9 +13,14 @@ func (c *compiler) compileForNode(v parser.ForNode) {
 
 	// Loop body block
 	loopBodyBlock := c.contextBlock.Parent.NewBlock(getBlockName() + "-body")
+	loopAfterBodyBlock := c.contextBlock.Parent.NewBlock(getBlockName() + "-after-body")
 
 	// After loop block
 	afterLoopBlock := c.contextBlock.Parent.NewBlock(getBlockName() + "-after")
+
+	// Push the break and continue stacks
+	c.contextLoopBreak = append(c.contextLoopBreak, afterLoopBlock)
+	c.contextLoopContinue = append(c.contextLoopContinue, loopAfterBodyBlock)
 
 	// Jump from BeforeLoop to checkCond
 	c.contextBlock.NewBr(checkCondBlock)
@@ -28,9 +33,25 @@ func (c *compiler) compileForNode(v parser.ForNode) {
 	// Compiler loop body
 	c.contextBlock = loopBodyBlock
 	c.compile(v.Block)
+	c.contextBlock.NewBr(loopAfterBodyBlock) // Jump to after body
+
+	// After body block
+	c.contextBlock = loopAfterBodyBlock
 	c.compile([]parser.Node{v.AfterIteration})
 	c.contextBlock.NewBr(checkCondBlock)
 
 	// Set context to the new block after the loop
 	c.contextBlock = afterLoopBlock
+
+	// Pop break and continue
+	c.contextLoopBreak = c.contextLoopBreak[0 : len(c.contextLoopBreak)-1]
+	c.contextLoopContinue = c.contextLoopContinue[0 : len(c.contextLoopContinue)-1]
+}
+
+func (c *compiler) compileBreakNode(v parser.BreakNode) {
+	c.contextBlock.NewBr(c.contextLoopBreak[len(c.contextLoopBreak)-1])
+}
+
+func (c *compiler) compileContinueNode(v parser.ContinueNode) {
+	c.contextBlock.NewBr(c.contextLoopContinue[len(c.contextLoopContinue)-1])
 }
