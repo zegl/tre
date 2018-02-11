57a2976dd38a81ff453c5534f78bfcad2978c1db
Gustav Westling <hello@gustav.tv>
2018-02-11 10:48:14 +0100

    fix(parser) improve comments and interfaces for nodes

---
+++ compiler/parser/node_types.go
@@ -0,0 +1,32 @@
+package parser
+
+import "fmt"
+
+// TypeNode is an interface for different ways of creating new types or refering to existing ones
+type TypeNode interface {
+	Node() // must also implement the Node interface
+	Type() string
+}
+
+// SingleTypeNode refers to an existing type. Such as "string".
+type SingleTypeNode struct {
+	baseNode
+
+	TypeName string
+}
+
+func (stn *SingleTypeNode) Type() string {
+	return stn.TypeName
+}
+
+// StructTypeNode refers to a struct type
+type StructTypeNode struct {
+	baseNode
+
+	Types []TypeNode
+	Names map[string]int
+}
+
+func (stn *StructTypeNode) Type() string {
+	return fmt.Sprintf("%+v", stn.Types)
+}
