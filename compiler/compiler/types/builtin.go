7bcd20fc509d1b87eb8b274476af0b85bd9d6eb6
Gustav Westling <hello@gustav.tv>
2018-04-28 15:56:32 +0200

    ref(types) remove the Basic type, added more specific types instead

--- compiler/compiler/types/builtin.go
+++ compiler/compiler/types/builtin.go
@@ -2,13 +2,12 @@ package types
 
 import "github.com/llir/llvm/ir/types"
 
-var I1 = &Basic{Type: types.I1, TypeName: "bool", TypeSize: 1}
-var I8 = &Basic{Type: types.I8, TypeName: "int8", TypeSize: 1}
-var I16 = &Basic{Type: types.I16, TypeName: "int16", TypeSize: 2}
-var I32 = &Basic{Type: types.I32, TypeName: "int32", TypeSize: 4}
-var I64 = &Basic{Type: types.I64, TypeName: "int64", TypeSize: 8}
-var Bool = &Basic{Type: types.I1, TypeName: "bool"}
-var Void = &Basic{Type: types.Void, TypeName: "void"}
-var String = &StringType{}
+var I1 = &Int{Type: types.I1, TypeName: "bool", TypeSize: 1}
+var I8 = &Int{Type: types.I8, TypeName: "int8", TypeSize: 1}
+var I16 = &Int{Type: types.I16, TypeName: "int16", TypeSize: 2}
+var I32 = &Int{Type: types.I32, TypeName: "int32", TypeSize: 4}
+var I64 = &Int{Type: types.I64, TypeName: "int64", TypeSize: 8}
 
-var _ Type = I8
+var Void = &VoidType{}
+var String = &StringType{}
+var Bool = &BoolType{}
