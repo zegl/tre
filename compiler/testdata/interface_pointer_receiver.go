ad906d4a1219a9df11ffe560a63e333c84111942
Gustav Westling <hello@gustav.tv>
2018-06-04 21:41:58 +0200

    fea(interface) use method from parent type when using pointer receivers

--- compiler/testdata/interface_pointer_receiver.go
+++ compiler/testdata/interface_pointer_receiver.go
@@ -20,11 +20,27 @@ func (f *FaceImpl) Add(val int) int {
 
 func main() {
 	var face TestFace
-	face = &FaceImpl{}
 
+	impl := FaceImpl{}
+	face = impl
+
+	// val: 3
+	// res: 3
 	res := face.Add(3)
 	external.Printf("res: %d\n", res)
 
+	// val: 4
+	// res: 7
 	res = face.Add(4)
+	external.Printf("res: %d\n", res)
+
+	// val: 1
+	// res: 8
+	res = face.Add(1)
+	external.Printf("res: %d\n", res)
+
+	// val: 5
+	// res: 13
+	res = face.Add(5)
 	external.Printf("res: %d\n", res)
 }
