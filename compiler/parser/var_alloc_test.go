c9ecea19d593b2c3af0e4edd4e1145a08a1c8488
Gustav Westling <gustav@westling.dev>
2020-02-02 12:30:55 +0100

    parser: support for parsing constants
    Very similar to the way variables are parsed

--- compiler/parser/var_alloc_test.go
+++ compiler/parser/var_alloc_test.go
@@ -98,3 +98,52 @@ func TestAllocGroup(t *testing.T) {
 
 	assert.Equal(t, expected, Parse(lexed, false))
 }
+
+func TestConstAlloc(t *testing.T) {
+	lexed := lexer.Lex(`const a = 30`)
+	expected := FileNode{
+		Instructions: []Node{
+			&AllocNode{
+				IsConst: true,
+				Name:    []string{"a"},
+				Val:     []Node{&ConstantNode{Type: NUMBER, Value: 30}},
+			},
+		},
+	}
+
+	assert.Equal(t, expected, Parse(lexed, false))
+}
+
+func TestAllocConstGroup(t *testing.T) {
+	lexed := lexer.Lex(`const (
+	a = 10
+	b, c = "bbb", "ccc"
+	d = 20
+)`)
+
+	expected := FileNode{
+		Instructions: []Node{
+			&AllocGroup{
+				Allocs: []*AllocNode{
+					{
+						IsConst: true,
+						Name:    []string{"a"},
+						Val:     []Node{&ConstantNode{Type: NUMBER, Value: 10}},
+					},
+					{
+						Name:    []string{"b", "c"},
+						Val:     []Node{&ConstantNode{Type: STRING, ValueStr: "bbb"}, &ConstantNode{Type: STRING, ValueStr: "ccc"}},
+						IsConst: true,
+					},
+					{
+						Name:    []string{"d"},
+						Val:     []Node{&ConstantNode{Type: NUMBER, Value: 20}},
+						IsConst: true,
+					},
+				},
+			},
+		},
+	}
+
+	assert.Equal(t, expected, Parse(lexed, false))
+}
